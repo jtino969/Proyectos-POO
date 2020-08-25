b086d605f9426e02a0d7f36b2af74e44a5df0054
Jorge Tinoco <62453117+jtino969@users.noreply.github.com>
2020-08-24 23:29:23 -0600

    Add files via upload

---
+++ POO/Proyecto Final de POO/JuegodeDados/src/main/java/JuegoDados/ImagenIcon.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package JuegoDados;
+
+import java.net.URL;
+
+/**
+ *
+ * @author personal
+ */
+public class ImagenIcon {
+
+    public ImagenIcon(URL resource) {
+    }
+
+    ImagenIcon(URL resource) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    }
+    
+}
